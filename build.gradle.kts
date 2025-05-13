f21df427798f6bf64f3295524701720171e0d91a
Matthias Geisler <bitpogo@antibytes.tech>
2025-05-13 06:00:12 +0200

    Add initial Project setup
    This adds the inital project setup, with Gradle, version catalogs and
    one module.

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    alias(dependencyCatalog.plugins.android.application) apply false
+    alias(dependencyCatalog.plugins.kotlin.android) apply false
+    alias(dependencyCatalog.plugins.kotlin.compose) apply false
+}

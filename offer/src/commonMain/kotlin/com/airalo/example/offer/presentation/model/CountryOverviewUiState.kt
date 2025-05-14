5b787c3576b19f2d08dd304ebfd479d159b02cda
Matthias Geisler <bitpogo@antibytes.tech>
2025-05-14 09:28:01 +0200

    Add OverviewUiState
    This models very crude UI states to have point of start for the UI.

---
+++ offer/src/commonMain/kotlin/com/airalo/example/offer/presentation/model/CountryOverviewUiState.kt
@@ -0,0 +1,36 @@
+package com.airalo.example.offer.presentation.model
+
+import com.airalo.example.offer.domain.entity.Country
+
+/**
+ * States for the Country Offer Overview of the UI.
+ *
+ * @property countries The list of countries to be used by the UI.
+ */
+sealed class CountryOverviewUiState(open val countries: List<Country>) {
+    /**
+     * The initial state before any data is loaded.
+     */
+    data object Initial : CountryOverviewUiState(emptyList())
+
+    /**
+     * Represents the state when the data is being loaded.
+     *
+     * @property countries The list of countries carried over from the previous state.
+     */
+    data class Loading(override val countries: List<Country>) : CountryOverviewUiState(countries)
+
+    /**
+     * Represents the state when the data has been successfully loaded.
+     *
+     * @property countries The list of countries freshly resolved from the interactor.
+     */
+    data class Success(override val countries: List<Country>) : CountryOverviewUiState(countries)
+
+    /**
+     * Represents an state of error which happened while loading the data.
+     *
+     * @property countries The list of countries carried over from the previous state.
+     */
+    data class Error(override val countries: List<Country>) : CountryOverviewUiState(countries)
+}

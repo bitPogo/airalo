2acda730c74f11b0e81a9043045e9638fb44c96a
Matthias Geisler <bitpogo@antibytes.tech>
2025-05-14 22:24:58 +0200

    Add CountryName

---
+++ offer/src/androidMain/kotlin/com/airalo/example/offer/presentation/ui/atom/Text.kt
@@ -0,0 +1,19 @@
+package com.airalo.example.offer.presentation.ui.atom
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import com.airalo.example.offer.presentation.ui.token.Font.overviewCountryName
+
+@Composable
+fun CountryName(content: String) {
+    Text(
+        content,
+        textAlign = TextAlign.Left,
+        style = overviewCountryName,
+        modifier = Modifier
+            .fillMaxSize()
+    )
+}

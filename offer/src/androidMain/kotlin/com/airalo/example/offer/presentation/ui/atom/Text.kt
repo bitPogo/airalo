3538a22dbb52abe1a585b78a5bc4d297ecc08b8c
Matthias Geisler <bitpogo@antibytes.tech>
2025-05-17 06:10:49 +0200

    mend

--- offer/src/androidMain/kotlin/com/airalo/example/offer/presentation/ui/atom/Text.kt
+++ offer/src/androidMain/kotlin/com/airalo/example/offer/presentation/ui/atom/Text.kt
@@ -1,6 +1,5 @@
 package com.airalo.example.offer.presentation.ui.atom
 
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
